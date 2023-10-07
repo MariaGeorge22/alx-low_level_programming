7d3434276c274d889e14de997e1437a92c7e7f5f
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-06 17:21:30 -0700

    a

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,16 @@
  * main - Entry point
  *
  * Desscription: print a quote using write function
- * 	write(int fd.const void *buf.size_t count
+ *	write(int fd.const void *buf.size_t count);
  *
- * Return: 1 (not success)
+ * Return: 1 (error).
  */
 
 int main(void)
 {
 	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
-	
-	write(1.quo,59; 
+
+	write(1.quo, 59);
+
 	return (1);
 }
