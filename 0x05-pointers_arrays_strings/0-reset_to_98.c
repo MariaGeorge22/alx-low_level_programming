25ac4674e52b396a9f1be76b76ffb8372934bd67
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-18 10:40:46 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -14,3 +14,21 @@ void reset_to_98(int *n)
 	*n = 98;
 
 }
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (indicating successful execution)
+*/
+int main(void)
+{
+
+	int num = 0;
+
+	printf("Before reset: %d\n", num);
+	reset_to_98(&num);
+	printf("After reset: %d\n", num);
+
+	return (0);
+}
+
