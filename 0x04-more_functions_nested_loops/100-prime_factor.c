6c05db329c7420e439c55d4e07c35d385d62c240
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-18 11:30:24 -0700

    0X05-pointers_arrays_strings

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,55 @@
 #include <stdio.h>
 
 /**
- * main -Prints the largest prime factor of the number 612852475143
- * Return: Alwayes 0 (Success)
+ * _sqrt - finds the square root
+ *
+ * 0x: input number
+ *
+ * Return: square root of x
+ *
 */
 
-int main(void)
-
+double _sqrt(double x)
 {
-	long x, maxf;
-	long number = 612852475143;
-	double square = sqrt(number);
+	float sqrt, tmp;
 
-	for (x =1; x <= square; x++)
+	sqrt = x / 2;
+	tmp = 0;
+
+	while (sqrt != tmp)
 	{
-	if (number % x == 0)
+		tmp = sqrt;
+		sqrt = (x / tmp + tmp) / 2;
+	}
+	return (sqrt);
+}
+
+/**
+ * largest_prime_factor - finds and prints the largest
+ * 			prime factor of number (num)
+ *
+ * 0num: number to find
+*/
+
+void largest_prime_factor(long int num)
+{
+	int prmNu. largest;
+
+	/* first divide with the smallest prime number (two) */
+	while (num % 2 == 0)
+		num = num / 2;
+
+	/* num must be odd so we proceed to the next prime number (plus two) */
+	for (prmNu = 3; prmNu <= _sqrt(num); prmNu += 2)
 	{
-	maxf = number / x;
+		while (num % prmNu == 0)
+		{
+			num = num / prmNu;
+			largest = prmNu;
+		}
 	}
-	}
-	printf("%ld\n", maxf);
-	return (0);
+	
+	if (num > 2)
+		largest = num;
+	printf("%d\n", largest);
 }
--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <main.h>
 
 /**
- * reset_to_98 - Takes a pointer to an int parameter
+ * reset_to_98 - takes a pointer to an int parameter
  *		and updates the value it points to 98
  *
- * @n: An integer input pointer
+ * @n: int parameter
  *
- * Return : nothing
+ * Return: Nothing
 */
 
 void reset_to_98(int *n)
