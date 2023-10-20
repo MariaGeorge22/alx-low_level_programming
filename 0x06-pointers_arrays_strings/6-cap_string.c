aea72ba9615ce00471ce589a717046e8164182e3
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-19 18:44:16 -0700

    6-cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * islower - determines whether ascii is lowercase
+ * @c: character
+ * Return: 1 if true, 0 if false
+*/
+
+int islower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - determine whether ascii is a delimiter
+ * @c: character
+ * Return: 1 if true, 0 if false
+*/
+
+int isDelimiter(char c)
+{
+	int i;
+	char delimiter[] = " \t\n,.!?\"()()";
+
+	for (i = 0; i < 12; i++)
+		if (c == delimiter[i])
+			return (1);
+
+	return (0);
+}
