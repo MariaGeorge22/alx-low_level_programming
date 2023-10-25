3fc56bc675aab8f158c3f96dfaf2acd5aa881be9
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-25 11:40:55 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,36 +1,36 @@
 #include "main.h"
 
 /**
- *  * temp_print - temp function.
- *   * @str: pointer to a string
- *    *
- *     * prints a string in reverse
- *      * through recursion
- *       *
- *        * Return: void
+ * temp_print - temp function.
+ * @str: pointer to a string
+ *
+ * prints a string in reverse
+ * through recursion
+ *
+ * Return: void
 */
 void temp_print(char *str)
 {
-		char *next;
+	char *next;
 
-			if (*str != '\0')
-					{
-								next = str + 1;
-										_putchar(*str);
-												temp_print(next);
-													}
+	if (*str != '\0')
+	{
+	next = str + 1;
+	_putchar(*str);
+	temp_print(next);
+	}
 }
 /**
- *  * _puts_recursion - pointer function.
- *   * @s: pointer to a string
- *    *
- *     * prints a string, using recursion
- *      * followed by a new line
- *       *
- *        * Return: void
+ * _puts_recursion - pointer function.
+ * @s: pointer to a string
+ *
+ * prints a string, using recursion
+ * followed by a new line
+ *
+ * Return: void
 */
 void _puts_recursion(char *s)
 {
-		temp_print(s);
-			_putchar('\n');
+	temp_print(s);
+	_putchar('\n');
 }
