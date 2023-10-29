2c9791e5a19b99b67be2e94578c9c39bffcd58dd
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-29 09:29:09 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check function
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * prints its number of arguments
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char const *argv[])
+{
+		(void)argv;
+			printf("%d\n", argc - 1);
+				return (0);
+}
