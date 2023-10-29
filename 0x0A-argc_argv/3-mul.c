22ea14cd4b5f8406285c38f93280f9f9053128cd
MariaGeorge22 <maria.grorge2222@gmail.com>
2023-10-29 09:41:28 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check function
+ * @argc: number of arguments
+ * @argv: arguments
+ *
+ * prints its number of arguments
+ *
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char const *argv[])
+{
+	if (argc != 3)
+	{
+	printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
