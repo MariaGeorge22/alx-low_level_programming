301c4e70f439360249176d1422212c6c1720acde
MariaGeorge22 <maria.george2222@gmail.com>
2024-01-15 14:59:19 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P "../libgiga.so" "https://github.com/MariaGeorge22/alx-low_level_programming//raw/main/0x18-dynamic_libraries/libgiga.so"
-export LD_PRELOAD="~/../libgiga.so"
+wget -P "../libgiga.so" "https://github.com/MariaGeorge22/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so"
+export LD_PRELOAD="../libgiga.so"
