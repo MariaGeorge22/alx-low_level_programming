edb8e0ba74bb1bd7d05ea9b222a05335613d9b6c
MariaGeorge22 <maria.george2222@gmail.com>
2024-01-15 14:40:46 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp  https://github.com/MariaGeorge22e/alx-low_level_programming/raw/master/0x18-dynamic_libraries/101-fake_rand.so
-export LD_PRELOAD=/tmp/libgiga.so
+wget -O "../101-fake_rand.so" "https://raw.githubusercontent.com/MariaGeorge22/alx-low_level_programming/main/0x18-dynamic_libraries/101-fake_rand.so"
+export LD_PRELOAD="$PWD/../101-fake_rand.so"
