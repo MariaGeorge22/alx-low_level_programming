29dc11294e48e3d0605b9b488f1072669f22f813
MariaGeorge22”git config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-05-20 02:08:39 -0700

    0-linear.c

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+ *  * linear_search - search alg
+ *   * @array: array
+ *    * @size: size
+ *     * @value: value
+ *      *
+ *       * searches for a value in an array of integers
+ *        * using the Linear search algorithm
+ *         *
+ *          * Return: index of value or -1
+ *           */
+int linear_search(int *array, size_t size, int value)
+{
+		size_t index = -1, i;
+
+			if (array != NULL)
+					{
+								for (i = 0; i < size; i++)
+											{
+															printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+																		if (array[i] == value)
+																						{
+																											index = i;
+																															break;
+																																		}
+																				}
+									}
+
+				return (index);
+}
