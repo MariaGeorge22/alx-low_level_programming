f001ffe2654203a5c6fb73d4a2c18c40edc11d14
MariaGeorge22”git config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-05-20 02:26:00 -0700

    0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,20 +12,19 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
-		size_t index = -1, i;
+	size_t index = -1, i;
 
-			if (array != NULL)
-					{
-								for (i = 0; i < size; i++)
-											{
-															printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-																		if (array[i] == value)
-																						{
-																											index = i;
-																															break;
-																																		}
-																				}
-									}
-
-				return (index);
+	if (array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+			{
+				index = i;
+				break;
+			}
+		}
+	}
+	return (index);
 }
